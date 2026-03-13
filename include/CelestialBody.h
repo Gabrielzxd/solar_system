b6c6564ee60587ddefb6dacd5980eb8db29f52cc
Gabriel Silva <gabbrielssilva.biel@gmail.com>
2026-03-13 09:59:49 -0400

    melhora no visual do sistema solar

--- include/CelestialBody.h
+++ include/CelestialBody.h
@@ -1,17 +1,26 @@
 #pragma once
 #include "Vector.h"
+#include <iostream>
+
+struct Color{
+    float r, g, b;
+};
 
 class CelestialBody {
 private:
+    std::string name;
     Vector position;
     Vector velocity;
     double mass;
     double radius;
     Vector force_accumulator;
+    Color color;
+
 public:
-    CelestialBody();
-    CelestialBody(const Vector& position, const Vector& velocity, double mass, double radius);
+    CelestialBody(std::string name, const Vector& position, const Vector& velocity, double mass, double radius, Color RGB);
     Vector getPosition() const;
+    void setName(const std::string& new_name);
+    std::string getName() const;
     void setPosition(const Vector& new_position);
     Vector getVelocity() const;
     void setVelocity(const Vector& new_velocity);
@@ -21,4 +30,5 @@ public:
     Vector getGravitationalForce(const CelestialBody& other) const;
     Vector getAcceleration(const CelestialBody& other) const;
     void update(double dt);
+    Color getColor() const;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,36 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 #include "../include/CelestialBody.h"
+#include <cmath>
 
 int main() {
-    // 1. Setup Inicial do OpenGL (Igual ao anterior)
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Simulacao N-Corpos", NULL, NULL);
+
+    GLFWmonitor* primary = glfwGetPrimaryMonitor();
+
+    const GLFWvidmode* mode = glfwGetVideoMode(primary);
+
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Simulação Sistema Solar", NULL, NULL);
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    // 2. Criar os corpos (Sua física)
+    float aspectRatio = (float)mode->width / (float)mode->height;
+
     std::vector<CelestialBody> bodies;
-    bodies.emplace_back(Vector{0.0, 0.0}, Vector{0.0, 0.0}, 100.0, 1.0); // Sol
-    bodies.emplace_back(Vector{10.0, 0.0}, Vector{0.0, 3.16}, 1.0, 0.1); // Terra
+    bodies.emplace_back("Sol", Vector{0.0, 0.0}, Vector{0.0, 0.0}, 1.0, 0.8, Color{1.0f, 0.9f, 0.0f});
+    bodies.emplace_back("Mercurio", Vector{0.387, 0.0}, Vector{0.0, 10.09}, 1.65e-7, 0.1, Color{0.6f, 0.6f, 0.6f});
+    bodies.emplace_back("Venus", Vector{0.723, 0.0}, Vector{0.0, 7.39}, 2.45e-6, 0.2, Color{0.9f, 0.8f, 0.6f});
+    bodies.emplace_back("Terra", Vector{1.000, 0.0}, Vector{0.0, 6.28}, 3.00e-6, 0.2, Color{0.2f, 0.4f, 1.0f});
+    bodies.emplace_back("Marte", Vector{1.524, 0.0}, Vector{0.0, 5.08}, 3.23e-7, 0.15, Color{0.9f, 0.3f, 0.1f});
+    bodies.emplace_back("Jupiter", Vector{5.204, 0.0}, Vector{0.0, 2.75}, 9.54e-4, 0.5, Color{0.8f, 0.6f, 0.4f});
+    bodies.emplace_back("Saturno", Vector{9.582, 0.0}, Vector{0.0, 2.05}, 2.85e-4, 0.4, Color{0.9f, 0.8f, 0.5f});
+    bodies.emplace_back("Urano", Vector{19.201, 0.0}, Vector{0.0, 1.43}, 4.36e-5, 0.3, Color{0.6, 0.9f, 0.9f});
+    bodies.emplace_back("Netuno", Vector{30.047, 0.0}, Vector{0.0, 1.14}, 5.15e-5, 0.3, Color{0.2f, 0.3f, 0.9f});
 
     double dt = 0.01;
-    float zoom = 15.0f; // Fator para caber na tela
+    float zoom = 40.0f;
 
     while (!glfwWindowShouldClose(window)) {
-        // --- LOGICA DE FISICA ---
         for(size_t i = 0; i < bodies.size(); i++) {
             for(size_t j = 0; j < bodies.size(); j++) {
                 if(i != j) bodies[i].attract(bodies[j]);
@@ -31,16 +43,25 @@ int main() {
         glClearColor(0.05f, 0.05f, 0.1f, 1.0f); // Fundo quase preto
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Desenhar os corpos como pontos grandes (depois faremos circulos)
+        Vector sunPos = bodies[0].getPosition();
+
+        // Desenhar os corpos
         glPointSize(10.0f);
-        glBegin(GL_POINTS);
+        float visual_scale = 0.03f;
         for (const auto& body : bodies) {
             Vector p = body.getPosition();
-            // Convertendo a posicao da fisica para a escala do OpenGL (-1 a 1)
-            glVertex2f(p.x / zoom, p.y / zoom);
+            Color colors = body.getColor();
+            glColor3f(colors.r, colors.g, colors.b);
+            glBegin(GL_POLYGON);
+            for (int i = 0; i < 60; i++){
+                float theta = 2.0f * 3.1415926f * i / 60.0f;
+                float x = body.getRadius() * cos(theta) * visual_scale;
+                float y = body.getRadius() * sin(theta) * visual_scale;
+                glVertex2f( (((p.x - sunPos.x) / zoom) + x) / aspectRatio, ((p.y - sunPos.y) / zoom) + y );
+            }
+            glEnd();
         }
-        glEnd();
-
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
